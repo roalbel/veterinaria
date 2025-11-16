77ae6977e61cb292b855c4a4cc191fe34eb1ae71
Cesarmendez-code <eliezellebronroalbel@gmail.com>
2025-11-16 02:32:06 -0400

    add
    add

---
+++ Veteriania.API/Controllers/PropietarioController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using veterinaria.infretruture.DBContex;
+using veterinaria.infretruture.Model;
+using Veteriania.API.DTOS;
+using tallermecanico.domain.Entityes;
+
+namespace CRUD_API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class PropietarioController : ControllerBase
+    {
+        private readonly VeterinariaContex _context;
+
+        public PropietarioController(VeterinariaContex context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPropietarios()
+        {
+            var propietarios = _context.Propietarios.ToList();
+
+            var dtoList = propietarios.Select(p => new PropietarioDto
+            {
+                
+                Nombre = p.Nombre,
+                Cedula = p.Cedula,
+                Telefono = p.Telefono,
+                Direccion = p.Direccion
+            }).ToList();
+
+            return Ok(dtoList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPropietarioById(int id)
+        {
+            var propietario = _context.Propietarios.FirstOrDefault(p => p.PropietatioId == id);
+            if (propietario == null)
+                return NotFound();
+
+            return Ok(propietario);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePropietario([FromBody] PropietarioDto dto)
+        {
+            var propietario = new Propietario
+            {
+                Nombre = dto.Nombre,
+                Cedula = dto.Cedula,
+                Telefono = dto.Telefono,
+                Direccion = dto.Direccion
+            };
+
+            _context.Add(propietario);
+            _context.SaveChanges();
+
+            return Ok(dto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePropietario(int id, [FromBody] PropietarioDto dto)
+        {
+            var propietario = _context.Propietarios.FirstOrDefault(p => p.PropietatioId == id);
+            if (propietario == null)
+                return NotFound();
+
+            propietario.Nombre = dto.Nombre;
+            propietario.Cedula = dto.Cedula;
+            propietario.Telefono = dto.Telefono;
+            propietario.Direccion = dto.Direccion;
+
+            _context.Update(propietario);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePropietario(int id)
+        {
+            var propietario = _context.Propietarios.FirstOrDefault(p => p.PropietatioId == id);
+            if (propietario == null)
+                return NotFound();
+
+            _context.Propietarios.Remove(propietario);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Veteriania.API/DTOS/MascotaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Veteriania.API.DTOS
+{
+    public class MascotaDto
+    {
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public string Raza { get; set; }
+        public int Edad { get; set; }
+        public int PropietarioId { get; set; }
+    }
+
+}
